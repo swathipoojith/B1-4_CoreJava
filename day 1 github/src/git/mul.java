dd6a9866b196418c0e34a8c09fcb11407fd96b70
Swathi <Swathi@DESKTOP-BVT9SUG>
2022-03-30 14:01:39 +0530

    first message

---
+++ day 1 github/src/git/mul.java
@@ -0,0 +1,16 @@
+package git;
+
+public class mul {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		a = 5;
+		b = 10;
+		c = a*b;
+		System.out.println("5*10 = " + c);
+
+	}
+
+}
