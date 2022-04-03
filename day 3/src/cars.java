6b47e5f2d3a7cd5116fc43545313b7c31ff9bc4f
Swathi <Swathi@DESKTOP-BVT9SUG>
2022-04-03 19:13:57 +0530

    First commit

---
+++ day 3/src/cars.java
@@ -0,0 +1,12 @@
+
+class intro {
+    public static void main(String[] args) {
+    	
+        System.out.println("1. audi ");
+        System.out.println("2. benz ");
+        System.out.println("3. bmw ");
+        
+    	
+     
+    }
+}
