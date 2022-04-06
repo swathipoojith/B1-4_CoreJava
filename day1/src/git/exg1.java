d8bf3f4bf78a0062f368b2dc0870a9646a3eaf24
Swathi <Swathi@DESKTOP-BVT9SUG>
2022-04-06 20:34:56 +0530

    First Commit

---
+++ day1/src/git/exg1.java
@@ -0,0 +1,10 @@
+package git;
+
+public class exg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day1/src/git/exg2.java
@@ -0,0 +1,11 @@
+package git;
+
+public class exg2 {
+	public static void main(String[] args) 
+	{
+		System.out.println("Swathi");
+		System.out.println("Poojith");
+
+	}
+
+}
