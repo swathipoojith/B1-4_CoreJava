e8d1906f4850abf1788d54e2678f98349be252a0
Swathi <Swathi@DESKTOP-BVT9SUG>
2022-04-07 08:42:37 +0530

    First Commit

---
+++ Day4/src/Packages/Example3.java
@@ -0,0 +1,18 @@
+package Packages;
+
+public class Example3 {
+
+	
+			public int balance;
+		public Example3(int bal)
+		{
+			balance=bal;
+		}
+		
+		public void display()
+		{
+			System.out.println(balance);
+		}
+		
+
+	}
---
+++ Day4/src/Packages/Package1.java
@@ -0,0 +1,11 @@
+package Packages;
+
+
+public class Package1 
+{
+	public void display()
+	{
+		System.out.println("WELCOME TO CONCEPT OF PACKAGES");
+		
+	}
+}
